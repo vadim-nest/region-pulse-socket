19d0fa25a96f14ad14313def32df04b11efa9cf6
Vadim G. <vadim@gne.me.uk>
2025-06-01 16:25:25 +0100

    add: client error msg

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -4,9 +4,13 @@ export function useSocket<T>(url: string) {
   const [data, setData] = useState<T | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket(url);
-    ws.onmessage = (ev) => setData(JSON.parse(ev.data));
-    return () => ws.close();
+    try {
+      const ws = new WebSocket(url);
+      ws.onmessage = (ev) => setData(JSON.parse(ev.data));
+      return () => ws.close();
+    } catch (error) {
+      console.log(error);
+    }
   }, [url]);
 
   return data;
